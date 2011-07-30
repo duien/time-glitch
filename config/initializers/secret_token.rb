35712b36ec1275a589fba44a2b0d410f6f0baaed
Emily Price <price.emily@gmail.com>
2011-07-30 13:20:11 -0400

    Create rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeGlitch::Application.config.secret_token = '5572c8eaf7bc06510b187820cd2fc914a3d403d92d391f93caf06e02cb97571372c05e408c85a6d170082dec41146a9a07140aac4de4b557c88cbbeeeee6ca23'
